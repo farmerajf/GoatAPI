b767b372c1af7924562e92ca2eb83b7c79c83704
Adam Farmer <adam.farmer@just-eat.com>
2016-04-30 20:22:43 +0100

    Tidied up project and added a BingSearchAPI client

---
+++ BingSearchAPIClient/BingSearchAPIClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BingSearchClient
+{
+    public class BingSearchAPIClient
+    {
+        private readonly string _apiKey;
+        private const string ApiBaseUri = "https://bingapis.azure-api.net/api/v5/images/search";
+
+        public BingSearchAPIClient(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public async Task<BingSearchResponse> SearchImagesAsync(string query)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiBaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+
+                var response = await client.GetAsync(ApiBaseUri + "?q=" + query);
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+                var bingSearchReponse = JsonConvert.DeserializeObject<BingSearchResponse>(json);
+                return bingSearchReponse;
+            }
+        }
+    }
+}
---
+++ BingSearchAPIClient/BingSearchResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingSearchClient
+{
+    public class BingSearchResponse
+    {
+        public List<Value> Values { get; set; }
+    }
+
+    public class Value
+    {
+        public string ThumbnailUrl { get; set; }
+    }
+}
